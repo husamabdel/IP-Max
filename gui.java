2fafae30cfc8e0d0f603f11eb6c347740084253f
husamabdel <husamabdalla98@gmail.com>
2022-06-21 07:56:44 -0400

    added gui file

---
+++ gui.java
@@ -0,0 +1,24 @@
+import java.net.*;
+import java.net.http.*;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.*;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.*;
+import java.awt.desktop.*;
+import javax.swing.*;
+
+public class gui extends JFrame{
+
+	private JPanel panel1;
+	private JLabel label1;
+	private JLabel label2;
+	private JTabbedPane pane1;
+	private JMenuBar bar;
+	
+	
+}
